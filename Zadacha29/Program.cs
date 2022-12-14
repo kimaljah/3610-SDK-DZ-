dfdc47f5d590cd5c677e2027a8a0006205ca9914
kimaljah <kimik-rasta@mail.ru>
2022-12-14 16:52:34 +0300

    Zadacha29 project

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+// 5 -> [1, 2, 5, 7, 19]
+// 3 -> [6, 1, 33]
+
+Console.Write("Введите длинну массива : ");
+int Array = int.Parse(Console.ReadLine());
+
+int[] randomArray = new int[Array];
+
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(-10,20);
+    Console.Write($" {randomArray[i]} "+" ");
+}
